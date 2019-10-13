6dffa3bccc9196a1d70e9d3eb1c5eb9c956b56a7
eugene <utyfdfrthby08@gmail.com>
2019-10-13 18:08:48 +0300

    from pc

---
+++ wdio.conf.js
@@ -0,0 +1,28 @@
+exports.config = {
+  hostname: "localhost",
+  port: 4444,
+  path: "/wd/hub",
+  specs: ["./tests/hw2.ts"],
+  sync: true,
+  services: ["selenium-standalone"],
+  capabilities: [
+    {
+      browserName: "chrome"
+    }
+  ],
+  baseUrl: "http://ip-5236.sunline.net.ua:38015",
+  framework: "mocha",
+  mochaOpts: {
+    ui: "bdd",
+    timeout: 120000
+  },
+  reporters: ['spec'],
+  before: function(capabilities, specs) {
+    process.env.TS_NODE_FILES = true;
+    require("ts-node").register();
+    const chai = require("chai"),
+    expect = chai.expect; // preference and tested with expect
+ 
+    chai.use(require("chai-sorted"));
+  }
+};
