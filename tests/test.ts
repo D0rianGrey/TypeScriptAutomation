b5502820756d6b093515374087cb7f7655df1156
eugene <utyfdfrthby08@gmail.com>
2020-01-05 21:08:50 +0200

    from home pc

--- tests/test.ts
+++ tests/test.ts
@@ -1,16 +1,9 @@
 describe('Protractor', function () {
 
     it('WDIO', function () {
-        browser.url('/about-us-i-1');
-       /* let body = $('body');
-        console.log(body.getText());*/
+        browser.url('/create_account');
+        $('input[name="firstname"]').setValue("TestFirstName");
+        $('input[name="lastname"]').setValue("TestLastName");
 
-        /*let paragraphs = $$("p");
-        paragraphs.forEach(p=> {
-            console.log(p.getText());
-        })*/
-
-        let element = $('#box-information');
-        console.log(element.getText());
     });
 });
